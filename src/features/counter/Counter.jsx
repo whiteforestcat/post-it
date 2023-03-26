feacd8abe44d71bb4f0247e5377338e47b078c7f
mrmuhdamir <whiteforestcat96@gmail.com>
2023-03-26 15:10:21 +0800

    creating first state and jsx

---
+++ src/features/counter/Counter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./counterSlice";
+
+const Counter = () => {
+  const count = useSelector((state) => state.counter.count); // creating state, useSelector is used to reference the state in counterSlice.js
+  const dispatch = useDispatch();
+  return (
+    <>
+        <p>{count}</p>
+        <div>
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+        </div>
+    </>
+  )
+};
+
+export default Counter;
